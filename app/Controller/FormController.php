a0295f2476876736b7e52f465c138c44561037c9
mayer-jp <symbee@symbee-no-MacBook-Pro.local>
2017-10-05 16:26:37 +0900

    納品用

--- app/Controller/FormController.php
+++ app/Controller/FormController.php
@@ -2,15 +2,11 @@
 App::uses('AppController', 'Controller');
 
 class FormController extends AppController{
-  //$this->request->isPost(); 非推奨
-
-  //public $scaffold;
 
   public function index(){
     if($this->request->is('post')){
       $this->Form->create();
       if($this->Form->save($this->request->data)){
-        //$this->Flash->success('送信しました');
         return $this->redirect(array('action' => 'send'));
       }else{
         $this->Flash->error('入力内容に誤りがあります');
